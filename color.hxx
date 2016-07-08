85d4ff673dd494535e60e497b004b37b5144b1be
Taylor C. Richberger <Taywee@gmx.com>
2016-07-08 01:04:15 -0600

    add README content

---
+++ color.hxx
@@ -0,0 +1,21 @@
+/* Copyright © 2016 Taylor C. Richberger <taywee@gmx.com>
+ * This code is released under the license described in the LICENSE file
+ */
+
+enum class Color {
+    Unknown, // Not used in final output.  Compiler will decide what to do with this.
+    Red,
+    Yellow,
+    Green,
+    Cyan,
+    Blue,
+    Magenta,
+    White, // These aren't used in the compiled output
+    Black
+};
+
+enum class Shade {
+    Light,
+    Normal,
+    Dark
+};
